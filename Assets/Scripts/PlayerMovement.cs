4ad5e585a9ef3dbd0c63dbba4560dc5544267688
KamilErtap <k.ertap32500@gmail.com>
2025-01-25 10:14:25 +0300

    a

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -16,6 +17,10 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+
+    public GameObject head;
+    private float coolDown = 0f;
+    private float coolUp = 0f;
 
     void Start()
     {
@@ -41,12 +46,66 @@ public class PlayerMovement : MonoBehaviour
             isRigth = -1;
         }
 
-
+        RaycastHit2D hit2 = Physics2D.Raycast(head.transform.position, Vector2.up, 0.3f, groundLayer);
         // Jump
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);     
+            coolUp = 0.25f;
         }
+
+        coolDown -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.E) && coolDown < 0)
+        {
+            coolDown = 10f;
+            coolUp = 2f;
+            head.transform.localScale = new Vector3(2, 2, 2);
+            rb.gravityScale = -0.125f;
+            Invoke("notFly", 4);
+        }
+
+        if (!isGrounded)
+        {
+            if (hit2.collider != null)
+            {
+                if (hit2.collider.tag == "platform")
+                {
+                    gameObject.GetComponent<Collider2D>().isTrigger = true;
+                    Invoke("triggerFalse", coolUp);
+                }
+            }
+        }
+
+
+
+        // aþaðý düþme
+        RaycastHit2D hit = Physics2D.Raycast(groundCheck.position, Vector2.down, 0.2f, groundLayer);
+        
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "platform" && Input.GetKeyDown(KeyCode.S))
+            {
+                gameObject.GetComponent<Collider2D>().isTrigger = true;
+                coolUp = 0f;
+                Invoke("triggerFalse",0.5f);
+            }
+        }
+
+        
     }
+
+    private void triggerFalse()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = false;
+    }
+
+    private void notFly()
+    {
+        head.transform.localScale = new Vector3(1, 1, 1);
+        rb.gravityScale = 1f;
+    }
+
+
 }
---
+++ Assets/Scripts/krakenController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class krakenController : MonoBehaviour
+{
+
+    public GameObject krakenHead;
+    public GameObject krakenTentacle;
+
+    public int TENTACLEhitCount = 4;
+    public int HEADhitCount = 5;
+    public int tentacleCount = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
